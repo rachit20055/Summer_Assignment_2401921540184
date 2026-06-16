842df6228e5eb25084a3de417ba1c0ac52e5a070
Rachit Singh <myra.2005.gitty@gmail.com>
2026-06-16 23:03:48 +0530

    Week 3, Day1

---
+++ Week_3/Day_1/Linked_List_Cycle.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode l1=head;
+        ListNode l2=head;
+        while(l2!=null&&l2.next!=null) {
+            l1=l1.next;
+            l2=l2.next.next;
+            if(l1==l2) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
